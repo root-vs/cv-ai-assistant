e17479ed451f67a60a3cfd4e379b79ebf7799e91
root-vs <valerioserino.info@gmail.com>
2025-12-24 17:42:12 +0100

    Update ask.js

--- api/ask.js
+++ api/ask.js
@@ -1,1 +1,16 @@
 
+export default async function (req, res) {
+  const { messages } = await req.json();
+
+  const response = await fetch("https://api.langbase.com/v1/pipes/run", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.API_KEY}`
+    },
+    body: JSON.stringify({ messages })
+  });
+
+  const data = await response.json();
+  return res.json(data);
+}
